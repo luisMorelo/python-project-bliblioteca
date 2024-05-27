3d176a513032786960bf8f82c0c8ac771559486f
luisMorelo <luisalmopi@gmail.com>
2024-05-27 15:49:16 -0500

    Primera entrega

---
+++ biblioteca.py
@@ -0,0 +1,5 @@
+class Biblioteca:
+    def __init__(self, libro, usuario, prestamo_libro):
+        self.libro = libro
+        self.usuario = usuario
+        self.prestamo_libro = prestamo_libro
---
+++ main.py
@@ -0,0 +1,7 @@
+from libro import Libro
+from biblioteca import Bilioteca
+
+
+if __name__ == "__main__":
+    objeto = Libro(libro=[], usuario=[], prestamo_libro=[], autor='')
+    objeto.agregar_libro()
